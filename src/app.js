7c1778a281a9f4ac15355084a847b32d63500549
TNougaillac <tom.nougaillac@hotmail.fr>
2022-11-30 15:10:19 +0100

    regex qui passe bien

--- src/app.js
+++ src/app.js
@@ -2,5 +2,12 @@ const prompt = require('prompt');
 console.log("Entrez le calcul que vous voulez effectuer : ");
 prompt.start();
 prompt.get('calcul', function (err, result) {
-    console.log(result.calcul);
+    scanCalculate(result.calcul);
 });
+
+function scanCalculate(calculate) {
+    console.log("calcul: " + calculate);
+    //const word = calculate.split('');
+    const chars = /[/+/-/*/%]/;
+    console.log(calculate.search(chars));
+}
